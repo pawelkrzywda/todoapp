df0d2e6b51795d8b8dc5aad5547232f7fec3d819
Pawel Krzywda <paw.krzywda@gmail.com>
2020-12-28 19:07:28 +0100

    Updates

---
+++ precache-manifest.30e3186cd14536dd4421efe75fa4349f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6cd38030f009ffecdb9",
+    "url": "/todoapp/static/css/main.0bab7230.chunk.css"
+  },
+  {
+    "revision": "d6cd38030f009ffecdb9",
+    "url": "/todoapp/static/js/main.d6cd3803.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/todoapp/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "a18aa4c3e2f3489ee9ab43b7d1bcc4c0",
+    "url": "/todoapp/index.html"
+  }
+];
